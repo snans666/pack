47fc62fdf261cd22d772450ae0de685d38bac408
nb <nans.biboulet@yahoo.com>
2019-01-06 15:13:52 +0100

    On branch master
    Changes to be committed:
    	new file:   LICENSE   << MIT license
    	modified:   setup.py  << long description from README

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='pack',
       version='0.1',
       description='test packaging',
+      long_description=long_description,
       url='https://github.com/snans666/pack',
       author='snans666',
       license='MIT',
